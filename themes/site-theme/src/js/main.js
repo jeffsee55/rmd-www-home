b0f38192e0c5907de548b7555904136263094e67
Animated-Eagle <sheddinghorribly@gmail.com>
2019-05-22 15:19:24 -0400

    first ocmmit

---
+++ themes/site-theme/src/js/main.js
@@ -0,0 +1,36 @@
+import styles from './../css/main.css';
+import $ from 'jquery';
+import AOS from 'aos';
+import './scriptTools/elevator.js';
+import './scriptTools/bootstrapDropdown.js';
+
+AOS.init({
+    disable: 'mobile',
+    once: true
+});
+
+$(function() {
+  $(".mobile-menu-button").click(function() {
+    $('.mobile-menu').addClass("mobile-menu-toggle");
+  });
+});
+
+$(function() {
+  $(".mobile-menu-button").click(function() {
+    $('body').addClass("overflow-hidden");
+  });
+});
+
+$(function() {
+  $(".close-menu").click(function() {
+    $('.mobile-menu').removeClass("mobile-menu-toggle");
+    $('body').removeClass("overflow-hidden");
+  });
+});
+
+$(function() {
+  $(".mobile-menu-item").click(function() {
+    $('.mobile-menu').removeClass("mobile-menu-toggle");
+    $('body').removeClass("overflow-hidden");
+  });
+});
